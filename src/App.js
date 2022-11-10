01706ed5f1527b7a85e5906b1599d17fe03127dc
AndreiaIsa <andreiagoncalvesrh@gmail.com>
2022-11-10 11:42:49 +0000

    created structure of the project

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="book">
+        <h1 className="dictionary">Dictionary</h1>
+      </header>
+      <section className="definition">searchDefinitions</section>
+      <footer>
+        <a
+          href="https://github.com/AndreiaIsa/Dictionary-React-Projects"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open sourced
+        </a>
+        by Andreia Gonçalves
+      </footer>
+    </div>
+  );
+}
+
+export default App;
