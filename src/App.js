4b1987b925a0a10529722c7733ef9e72f50fea68
AndreiaIsa <andreiagoncalvesrh@gmail.com>
2022-11-10 12:17:44 +0000

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
+
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="book">
-        <h1 className="dictionary">Dictionary</h1>
-      </header>
-      <section className="definition">searchDefinitions</section>
-      <footer>
-        <a
-          href="https://github.com/AndreiaIsa/dictionary-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open sourced
-        </a>
-        by Andreia Gonçalves
-      </footer>
+      <div className="container">
+        <header className="book">
+          <h1 className="dictionary">Dictionary</h1>
+        </header>
+        <section>
+          <Dictionary />
+        </section>
+        <footer>
+          <a
+            href="https://github.com/AndreiaIsa/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open sourced
+          </a>
+          by Andreia Gonçalves
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+    alert(`searching for ${keyword}`);
+  }
+
+  function handleKeywordChange(event) {
+    event.preventDefault();
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div classname="dictionary">
+      <form onSubmit={search}>
+        <input type="search" autofocus={true} onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
